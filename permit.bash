071f2b846e977191f4b29faecede654c864e21de
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 01:26:03 +0530

    Update permit.bash

--- permit.bash
+++ permit.bash
@@ -3,15 +3,15 @@
 setfacl -m g:AlphaQ:--- /home
 
 for i in {1..30}
-	do
-		if [[ $i -lt 10 ]]; then
-			sudo setfacl -m u:Jay_Jay:w /home/sysAd_0"$i"
-			sudo setfacl -m u:Jay_Jay:w /home/appDev_0"$i"
-			sudo setfacl -m u:Jay_Jay:w /home/webDev_0"$i"
+do
+	if [[ $i -lt 10 ]]; then
+		sudo setfacl -m u:Jay_Jay:w /home/sysAd_0"$i"
+		sudo setfacl -m u:Jay_Jay:w /home/appDev_0"$i"
+		sudo setfacl -m u:Jay_Jay:w /home/webDev_0"$i"
         else
-            sudo setfacl -m u:Jay_Jay:w /home/sysAd_"$i"
-            sudo setfacl -m u:Jay_Jay:w /home/appDev_"$i"
-	    sudo setfacl -m u:Jay_Jay:w /home/webDev_"$i"
+            	sudo setfacl -m u:Jay_Jay:w /home/sysAd_"$i"
+            	sudo setfacl -m u:Jay_Jay:w /home/appDev_"$i"
+	    	sudo setfacl -m u:Jay_Jay:w /home/webDev_"$i"
 	fi
 done
 
@@ -21,8 +21,8 @@ sec_per()
 	if [[ $3 -lt 10 ]]; then
 		sudo setfacl -m u:"$1"_0"$3":rw /home/"$1"_0"$2"
 	elif [[ $3 -eq 10 ]]; then
-        sudo setfacl -m u:"$1"_0"$3":rw /home/"$1"_0"$2"
-    else 
+        	sudo setfacl -m u:"$1"_0"$3":rw /home/"$1"_"$2"
+    	else 
 		sudo setfacl -m u:"$1"_"$3":r /home/"$1"_0"$2"
 	fi		
 }
@@ -42,21 +42,21 @@ usr_per()
 		sec_per "$1" "$2" "$2"
 	elif [[ ($2 -gt 10) && ($2 -le 20) ]]; then
 		for i in {1..10}
-			do
-				sec_per "$1" "$i" "$2"
+		do
+			sec_per "$1" "$i" "$2"
 		done
-			th_fr_per "$1" "$2" "$2"
+		th_fr_per "$1" "$2" "$2"
 	else
 		for i in {1..20}
-			do
-				if [[ i -le 10 ]]
-				then
-					sec_per "$1" "$i" "$2"
-				else	
-					th_fr_per "$1" "$i" "$2"
-				fi
+		do
+			if [[ i -le 10 ]]
+			then
+				sec_per "$1" "$i" "$2"
+			else	
+				th_fr_per "$1" "$i" "$2"
+			fi
 		done
-		th_fr_per	"$1" "$2" "$2"
+		th_fr_per "$1" "$2" "$2"
 	fi
 }
 
