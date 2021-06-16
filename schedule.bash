74e3fc74e3a28a3b99ea64cf4ea714ee4a5362e9
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 01:40:54 +0530

    Update schedule.bash

--- schedule.bash
+++ schedule.bash
@@ -1,21 +1,12 @@
 #!/bin/bash
 
 
-for i in {1..30}
+for i in {01..30}
 do
-    if [[ $i -lt 10 ]]; then 
-        sudo echo -e "Date\t\tTime" > "/home/sysAd_0$i/schedule.txt"
-        sudo echo -e "Date\t\tTime" > "/home/appDev_0$i/schedule.txt"
-        sudo echo -e "Date\t\tTime" > "/home/webDev_0$i/schedule.txt"
-    else
-        sudo echo -e "Date\t\tTime" > "/home/sysAd_$i/schedule.txt"
-        sudo echo -e "Date\t\tTime" > "/home/appDev_$i/schedule.txt"
-        sudo echo -e "Date\t\tTime" > "/home/webDev_$i/schedule.txt"
-    fi
+    sudo echo -e "Date\t\tTime" > "/home/sysAd_$i/schedule.txt"
+    sudo echo -e "Date\t\tTime" > "/home/appDev_$i/schedule.txt"
+    sudo echo -e "Date\t\tTime" > "/home/webDev_$i/schedule.txt"
 done
-
-
-
 
 File="/home/Jay_Jay/future.txt"
 
@@ -26,16 +17,10 @@ sudo echo -e "Date\t\tTime" > "schedule.txt"
 date=`grep fd File` 
 
 if [[ "$date" != "" ]]; then
-    for i in {1..30}
+    for i in {01..30}
     do
-        if [[ $i -lt 10 ]]; then 
-            sudo echo -e "$d\t$t" >> "/home/sysAd_0$i/schedule.txt"
-            sudo echo -e "$d\t$t" >> "/home/appDev_0$i/schedule.txt"
-            sudo echo -e "$d\t$t" >> "/home/webDev_0$i/schedule.txt"
-        else
-            sudo echo -e "$d\t$t" >> "/home/sysAd_$i/schedule.txt"
-            sudo echo -e "$d\t$t" >> "/home/appDev_$i/schedule.txt"
-            sudo echo -e "$d\t$t" >> "/home/webDev_$i/schedule.txt"
-       fi
+        sudo echo -e "$d\t$t" >> "/home/sysAd_$i/schedule.txt"
+        sudo echo -e "$d\t$t" >> "/home/appDev_$i/schedule.txt"
+        sudo echo -e "$d\t$t" >> "/home/webDev_$i/schedule.txt"
     done
 fi
