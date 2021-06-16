3e714261ebfbb734aaec4df6e6ffe2460e1684b9
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 01:38:56 +0530

    Update genUser.bash

--- genUser.bash
+++ genUser.bash
@@ -6,25 +6,15 @@ sudo adduser -force-badname --gecos "" Jay_Jay
 sudo usermod -G AlphaQ Jay_Jay
 sudo usermod -aG sudo Jay_Jay
 
-for i in {1..30}
-    do
-        if [[ $i -lt 10 ]]; then
-            sudo adduser -force-badname --disabled-password --gecos "" sysAd_0"$i"
-            sudo usermod -G AlphaQ sysAd_0"$i"
-            
-            sudo adduser -force-badname --disabled-password --gecos "" appDev_0"$i"
-            sudo usermod -G AlphaQ appDev_0"$i"
-            
-            sudo adduser -force-badname --disabled-password --gecos "" webDev_0"$i"
-            sudo usermod -G AlphaQ webDev_0"$i"
-        else
-            sudo adduser -force-badname --disabled-password --gecos "" sysAd_"$i"
-            sudo usermod -G AlphaQ sysAd_"$i"
-            
-            sudo adduser -force-badname --disabled-password --gecos "" appDev_"$i"
-            sudo usermod -G AlphaQ appDev_"$i"
-            
-            sudo adduser -force-badname --disabled-password --gecos "" webDev_"$i"
-            sudo usermod -G AlphaQ webDev_"$i"
-        fi
+for i in {01..30}
+do
+ sudo adduser -force-badname --disabled-password --gecos "" sysAd_"$i"
+ sudo usermod -G AlphaQ sysAd_"$i"           
+ 
+ sudo adduser -force-badname --disabled-password --gecos "" appDev_"$i"
+ sudo usermod -G AlphaQ appDev_"$i"           
+ 
+ sudo adduser -force-badname --disabled-password --gecos "" webDev_"$i"
+ sudo usermod -G AlphaQ webDev_"$i"
+
 done
