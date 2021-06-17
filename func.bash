a64b3487b583bd00306735c2d0b545ac6f3dadbf
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 17:44:17 +0530

    Add files via upload

---
+++ func.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+START="2019-09-27"
+
+line=$(grep "$START" attendance.log | awk '{print $1}')
+if [[ $line != "" ]]
+then
+	touch tmp.txt
+	prs=("$line")            
+	printf "%s\n" "${prs[@]}" > tmp.txt
+	usr=$(grep "_0" tmp.txt | tail -1)
+	
+	touch cd /home/$usr/$START_mom.txt
+	echo "This meeting we discussed about blah blah" >> /home/$usr/$START_mom.txt
+	echo "." >> cd /home/$usr/$START_mom.txt
+	echo "End of meet" >> cd /home/$usr/$START_mom.txt
+	
+	rm tmp.txt	
+fi
