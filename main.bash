4abda97d367f2687f40565ef6cb2cad52e1499d5
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 23:57:25 +0530

    Update main.bash

--- main.bash
+++ main.bash
@@ -4,4 +4,4 @@ echo alias permit='./home/Jay_Jay/permit.bash' >> .bashrc
 echo alias schedule='/home/Jay_Jay/schedule.bash' >> .bashrc
 echo alias attendance='/home/Jay_Jay/attendance.bash" >> .bashrc
 
-0 0 * * * "/bin/bash" "/schedule.bash"
+echo "0 0 * * * "/bin/bash" "/schedule.bash"" >> /etc/crontab
