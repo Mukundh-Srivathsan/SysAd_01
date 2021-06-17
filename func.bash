d8bc8b0de66f343bc3ffe2650e752c6567acfef8
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 23:48:35 +0530

    Update func.bash

--- func.bash
+++ func.bash
@@ -10,10 +10,10 @@ then
 	printf "%s\n" "${prs[@]}" > tmp.txt
 	usr=$(grep "_0" tmp.txt | tail -1)
 	
-	touch cd /home/$usr/$START_mom.txt
+	touch /home/$usr/$START_mom.txt
 	echo "This meeting we discussed about blah blah" >> /home/$usr/$START_mom.txt
 	echo "." >> cd /home/$usr/$START_mom.txt
-	echo "End of meet" >> cd /home/$usr/$START_mom.txt
+	echo "End of meet" >> /home/$usr/$START_mom.txt
 	
 	rm tmp.txt	
 fi
