5dee90e02498c0a519a774ea9b94c049bad36f5c
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-17 12:09:32 +0530

    Update schedule.bash

--- schedule.bash
+++ schedule.bash
@@ -3,16 +3,22 @@
 
 for i in {01..30}
 do
-    sudo echo -e "Date\t\tTime" > "/home/sysAd_$i/schedule.txt"
-    sudo echo -e "Date\t\tTime" > "/home/appDev_$i/schedule.txt"
-    sudo echo -e "Date\t\tTime" > "/home/webDev_$i/schedule.txt"
+	if [[ ! -f "/home/sysAd_$i/schedule.txt" ]]; then
+    	sudo echo -e "Date\t\tTime" > "/home/sysAd_$i/schedule.txt"
+    fi
+    
+    if [[ ! -f "/home/appDev_$i/schedule.txt" ]]; then
+    	sudo echo -e "Date\t\tTime" > "/home/appDev_$i/schedule.txt"
+    fi
+    
+    if [[ ! -f "/home/webDev_$i/schedule.txt" ]]; then
+    	sudo echo -e "Date\t\tTime" > "/home/webDev_$i/schedule.txt"
+	fi
 done
 
-File="/home/Jay_Jay/future.txt"
+File="future.txt"
 
 fd=`date +%Y-%m-%d`
-
-sudo echo -e "Date\t\tTime" > "schedule.txt" 
 
 date=`grep fd $File` 
 
