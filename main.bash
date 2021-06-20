59a6f0fd58cadb45029dff967e5f472b2fde9c21
Mukundh Srivathsan <83499540+Mukundh-Srivathsan@users.noreply.github.com>
2021-06-20 11:42:16 +0530

    Update main.bash

--- main.bash
+++ main.bash
@@ -3,5 +3,6 @@ echo alias genUser='./home/Jay_Jay/genUser.bash' >> /home/Jay_Jay/.bashrc
 echo alias permit='./home/Jay_Jay/permit.bash' >> /home/Jay_Jay/.bashrc
 echo alias schedule='./home/Jay_Jay/schedule.bash' >> /home/Jay_Jay/.bashrc
 echo alias attendance='./home/Jay_Jay/attendance.bash' >> /home/Jay_Jay/.bashrc
+echo alias genMoM='./home/Jay_Jay/genMoM.bash' >> /home/Jay_Jay/.bashrc
 
 echo 0 0 * * *  /home/Jay_Jay/schedule.bash >> /etc/crontab
